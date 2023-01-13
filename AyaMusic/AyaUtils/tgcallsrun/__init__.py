a5fda5c723fc0d75694d2462531e71822539dcdf
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-14 03:33:33 +0700

    Update __init__.py

--- AyaMusic/AyaUtils/tgcallsrun/__init__.py
+++ AyaMusic/AyaUtils/tgcallsrun/__init__.py
@@ -5,6 +5,6 @@ from .queues import (clear, get, is_empty, put, task_done)
 from .downloader import download
 from .downloader import download
 from .convert import convert
-from .yukki import run
-from .yukki import smexy as ASS_ACC
+from .aya import run
+from .aya import smexy as ASS_ACC
 smexy = 1
